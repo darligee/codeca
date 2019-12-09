317e24fc7b8094d129efecc0bd2bcbf90739defa
darligee <darli.gee@gmail.com>
2019-12-09 14:10:12 +0100

    Netbeans projetcs, projects and game built with JAVA

---
+++ src/codeca/Codeca.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codeca;
+
+/**
+ *
+ * @author Darlington
+ */
+public class Codeca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String a = "Lyyla";
+       String b = "dinta";
+       String c = a + " "+ b;
+       c = "<" + b + "." + a + "@dael.com>";
+       System.out.print(c);
+
+
+               
+               
+               
+               
+    }
+}
